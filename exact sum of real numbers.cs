f1655c9b438c83aab998c02a7c68c7534f617eb8
mtolev <mtolev@abv.bg>
2017-01-26 01:56:13 +0200

    Add files via upload

---
+++ exact sum of real numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            decimal n = 0;
+            for (int i = 0; i < chislo; i++)
+                n += decimal.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            Console.ReadLine();
+        }
+    }
+}
